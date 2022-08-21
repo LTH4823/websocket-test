98c6608412217a9147aa4d8f3e48c9f64092070c
LTH4823 <peter4823zx@naver.com>
2022-08-21 21:11:20 +0900

    Initial commit

---
+++ src/test/java/org/taerock/websockettest/WebsocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taerock.websockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
