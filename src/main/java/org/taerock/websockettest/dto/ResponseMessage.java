d95119b379e117137416e32096a9070049300606
LTH4823 <peter4823zx@naver.com>
2022-08-21 21:21:08 +0900

    2. add dto (Message, ResponseMessage)

---
+++ src/main/java/org/taerock/websockettest/dto/ResponseMessage.java
@@ -0,0 +1,16 @@
+package org.taerock.websockettest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseMessage {
+
+    private String messageContent;
+
+
+
+}
